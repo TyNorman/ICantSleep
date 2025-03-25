556d2db53acb179de1a51807caa52d5889be97f4
Tyler Norman <TylerNorman881@hotmail.com>
2025-03-25 14:35:19 -0400

    Timer Functionality and Styling
    Implemented Timer functionality to input target time and play/pause and stop. Styled Timer component.

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,9 +1,15 @@
 import { useRef, useState, useEffect } from 'react';
 import React from 'react';
 
-import { SlClock } from "react-icons/sl";
+import { SlClock, SlControlPlay, SlControlPause } from "react-icons/sl";
+import { FaStop } from "react-icons/fa6";
+
 
 import './Timer.css';
+
+interface TimerProps {
+
+}
 
 const Timer = () => {
 
@@ -12,25 +18,82 @@ const Timer = () => {
 
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(0);
+    const [isTimerRunning, setIsTimerRunning] = useState(false);
 
-    const runTimer = () => {
-        console.log("runTimer()");
+    //Input Event Handlers
+    const changeHours = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setHours(Number(event.target.value));
+        setSeconds(0);
     }
 
+    const changeMinutes = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setMinutes(Number(event.target.value));
+        setSeconds(0);
+    }
+
+    //Timer Start/Pause/Stop Functions
+    const runTimer = () => {
+        console.log("runTimer()");
+        if (hours != 0 || minutes != 0 || seconds != 0) {
+            setIsTimerRunning(true);
+        } else {
+            setIsTimerRunning(false);
+            window.alert("Please enter a valid time");
+        }
+
+    }
+
+    const pauseTimer = () => {
+        setIsTimerRunning(false);
+    }
+
+    const stopTimer = () => {
+        setIsTimerRunning(false);
+        setHours(0);
+        setMinutes(0);
+        setSeconds(0);
+    }
+
+    // Helper function to format time values
+    const formatTime = (value: number) => {
+        return value < 10 ? `0${value}` : value.toString();
+    };
+
     useEffect(() => {
-        
-      }, []);
+        let interval = 0;
+
+        if (isTimerRunning) {
+            interval = setInterval(() => {
+                if (seconds > 0)
+                    setSeconds((seconds) => seconds - 1);
+                else if (minutes > 0) {
+                    setMinutes((minutes) => minutes - 1);
+                    setSeconds(59);
+                }
+            else if (hours > 0) {
+                setHours((hours) => hours - 1);
+                setMinutes(59);
+                setSeconds(59);
+                }
+            }, 1000); //Run the interval every second
+        }
+    return () => clearInterval(interval);
+    } , [hours, minutes, seconds, isTimerRunning]);
 
     return(
         <div className="timer rounded-xl border-2 border-solid border-white bg-gradient-to-t from-slate-900 to-zinc-600">
                 <div className="timerInputLabel">Timer</div>
-                <div className="timerRemaining">00:00</div>
+                <div className="timerRemaining">{formatTime(hours)}:{formatTime(minutes)}:{formatTime(seconds)}</div>
                 <div className="timerInputArea">
-                    <input type="number" className="timerInput rounded-sm border-1 border-solid border-white bg-gradient-to-t from-stone-900 to-slate-700" defaultValue="0" min="0" max="23" ref={timerHours}/>
+                    <input type="number" value={formatTime(hours)} onChange={changeHours} ref={timerHours} className="timerInput rounded-sm border-1 border-solid border-white bg-gradient-to-t from-stone-900 to-slate-700" defaultValue="0" min="0" max="23"  />
                     <div className="timerColon">:</div>
-                    <input type="number" className="timerInput rounded-sm border-1 border-solid border-white bg-gradient-to-t from-stone-900 to-slate-700" defaultValue="0" min="0" max="59" ref={timerMinutes}/>
-                    <button className="timerButton" onClick={runTimer}>  <SlClock className="iconTimer"/>  </button>
-                    </div>
+                    <input type="number" value={formatTime(minutes)} onChange={changeMinutes} ref={timerMinutes} className="timerInput rounded-sm border-1 border-solid border-white bg-gradient-to-t from-stone-900 to-slate-700" defaultValue="0" min="0" max="59"  />
+                </div>
+                <div className="timerControls">
+                    {!isTimerRunning ? <button className="timerControlButton" onClick={runTimer}> <SlControlPlay className="iconTimerPlay"/> </button> : <button className="timerControlButton" onClick={pauseTimer}> <SlControlPause className="iconTimerPause"/> </button>}
+                    <button className="timerControlButton"> <FaStop className="iconTimerStop"/></button>
+                </div>
             </div>
     )
 }
--- src/components/VolumeSlider.tsx
+++ src/components/VolumeSlider.tsx
@@ -18,18 +18,6 @@ const VolumeSlider: React.FC<VolumeSliderProps> = ({ volume, setVolume })  => {
 
     const volumeBar = useRef<HTMLInputElement>(null); //Reference for the volume slider bar
 
-    /*
-    const changeVolume = () => { //Function to change the range slider
-        if (volumeBar.current) {
-            //Visually update the VolumeBar
-            var currentVolume = parseInt(volumeBar.current.value);
-            volumeBar.current.style.setProperty('--vol-before-width', `${currentVolume}%`); 
-
-            //TODO: Actually return a value to adjust the volume in AudioPlayer for something (ex. rain)
-            dispatchEvent(new CustomEvent('volumeChange', {detail: currentVolume})); 
-        }
-    }*/
-
     return(
         <div className="volumeSlider">
             <div>
